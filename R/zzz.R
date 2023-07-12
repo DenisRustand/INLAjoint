804c9adad8f3ee203bcf288a0a98ae2c6ae8a430
DenisRustand <denis@rustand.fr>
2023-07-12 17:27:49 +0300

    add startup message / version

---
+++ R/zzz.R
@@ -0,0 +1,24 @@
+# .onLoad <- function(libname, pkgname)
+# {
+#   library.dynam("INLAjoint", pkgname, libname)
+# }
+
+INLAjointStartupMessage <- function()
+{
+
+  msg <- c(paste0("INLAjoint version ",
+packageVersion("INLAjoint")),
+"\nType 'citation(\"INLAjoint\")' for citing this R package in publications.")
+  return(msg)
+}
+
+.onAttach <- function(lib, pkg)
+{
+  # startup message
+  msg <- INLAjointStartupMessage()
+    msg[1] <- paste("Package 'INLAjoint' version", packageVersion("INLAjoint"))
+  packageStartupMessage(msg)
+  invisible()
+}
+
+
