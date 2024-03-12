05746dd91c670c9df0826b42ee52b385dacf6f82
Elias T Krainski <eliaskrainski@gmail.com>
2024-03-12 10:08:37 +0300

    work on which argument to plot

--- R/print.plot.INLAjoint.R
+++ R/print.plot.INLAjoint.R
@@ -8,33 +8,98 @@
 #' linear associations and 'NL_Associations' or 'N' for non-linear associations. Baseline
 #' hazard curves referred to as 'Baseline' or 'B' and baseline hazard related parameters
 #' referred to as 'ParamBaseline' or 'P'. It is possible to select specific plots to
-#' print by specifying the names or corresponding lettes in the argument 'which'.
+#' print by specifying the names or corresponding letters in the argument 'which'.
 #'
 #' @param x an object with the output of the the \link{joint} function
-#' @param which name of required plots. Default is all.
-#'
+#' @param which name of required plots. Default is "all".
+#'  It can be a character or named list.
+#'  If it is a list, each element can be either character or numeric to
+#'  select from the elements to be visualized.
+#'  Ex.: which = list(Outcomes = "L1") and list(Outcomes = 1)
+#'  will produce the same output.
 #' @import ggplot2
 #' @importFrom grDevices dev.new
 #' @export
-print.plot.INLAjoint <- function(x, which="all", ...) {
-  if(which=="all"){
-    if(dev.interactive()){
-      oask <- devAskNewPage(TRUE)
-      on.exit(devAskNewPage(oask))
+print.plot.INLAjoint <- function(x,
+                                 which = c("all", "Outcomes", "Covariances",
+                                           "Associations", "Baseline"),
+                                 ...) {
+  stopifnot(class(which) %in% c("character", "list"))
+  if(dev.interactive()){
+    oask <- devAskNewPage(TRUE)
+    on.exit(devAskNewPage(oask))
+  }
+  wch <- c("all", "Outcomes", "Covariances", "Associations", "Baseline")
+  if(is.character(which)) {
+    which <- unique(which)
+    for(i in 1:length(which)) {
+      which[i] <- match.arg(which[i], wch)
     }
-    for(i in 1:length(x)) {
-      # if(i>1) dev.new()
-      if(all(class(x[[i]]) == "list")) {
-        for(j in 1:length(x[[i]])) {
-          print(x[[i]][[j]])
-          dev.flush()
+    if(any(which=="all")) {
+      iwhich <- vector("list", length(x))
+      iw1 <- 1:length(x)
+      for(i in iw1) {
+        if(all(class(x[[i]]) == "list")) {
+          iwhich[[i]] <- 1:length(x[[i]])
         }
-      } else {
-        print(x[[i]])
+      }
+    } else {
+      iw1 <- pmatch(which, wch[-1])
+      iw1 <- iw1[!is.na(iw1)]
+      if(length(iw1) == 0) {
+        stop('Invalid "which" argument!')
+      }
+      iwhich <- vector("list", length(iw1))
+      for(i in 1:length(iw1)) {
+        if(all(class(x[[i]]) == "list")) {
+          iwhich[[i]] <- 1:length(x[[iw1[i]]])
+        }
       }
     }
-  }else{
-    print("WIP")
+  }
+  if(is.list(which)) {
+    iw1 <- pmatch(names(which), wch[-1])
+    if(all(is.na(iw1))) {
+      stop('Invalid "names(which)"')
+    }
+    if(any(is.na(iw1))) {
+      warning('Some elements of "names(which)" are invalid"!')
+    }
+    iw1 <- iw1[!is.na(iw1)]
+    iwhich <- vector("list", length(iw1))
+    for (i in 1:length(which)) {
+      if(all(class(x[[i]]) == "list")) {
+        if(is.character(which[[i]])) {
+          ij <- pmatch(which[[i]], names(x[[iw1[i]]]))
+          if(all(is.na(ij))) {
+            warning('Found invalid element(s) in the element', i, 'of "which"!')
+          }
+          iwhich[[i]] <- ij[!is.na(ij)]
+        } else {
+          if(!is.numeric(which[[i]])) {
+              warning('Element', i, 'of "which" is invalid!')
+          } else {
+            iwhich[[i]] <- intersect(which[[i]], 1:length(x[[i]]))
+            if(length(iwhich[[i]])<length(which[[i]])) {
+                warning('Found invalid element(s) in the element', i, 'of "which"!')
+            }
+            if(length(iwhich[[i]])==0) {
+               warning('Element', i, 'of "which" is invalid!')
+            }
+          }
+        }
+      }
+    }
+  }
+  for(i in iw1) {
+    if(all(class(x[[i]]) == "list")) {
+      for(j in iwhich[[i]]) {
+        print(x[[i]][[j]])
+        dev.flush()
+      }
+    } else {
+      print(x[[i]])
+    }
   }
   devAskNewPage(FALSE)
 }
